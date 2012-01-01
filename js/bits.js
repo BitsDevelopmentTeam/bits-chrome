8a138fb7d7bde46e4f2e78733f078dc6325a107a
Andrea Brancaleoni <virus@gmx.us>
2012-01-01 01:24:48 +0100

    Working chrome extension

--- js/bits.js
+++ js/bits.js
@@ -1,4 +1,10 @@
-WS("ws://bits.poul.org:8080", function (openClose) {
+var ws = new WebSocket("ws://bits.poul.org:8080/");
+
+ws.onmessage = function(event) {
+  openCloseHandler(event.data);
+}
+
+var openCloseHandler = function (openClose) {
   if (openClose == "open") {
     chrome.browserAction.setIcon({path: "img/open.jpg"});
     chrome.browserAction.setTitle({title: "Poul Aperto"});
@@ -6,7 +12,7 @@ WS("ws://bits.poul.org:8080", function (openClose) {
     chrome.browserAction.setIcon({path: "img/close.jpg"});
     chrome.browserAction.setTitle({title: "POuL Chiuso"});
   }
-});
+}
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({url: "http://bits.poul.org"});
