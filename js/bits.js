76e1db0be9e8f9a83cfb1c9b9194d7cf1f25222e
Andrea Brancaleoni <virus@gmx.us>
2012-01-05 12:17:27 +0100

    chrome JSON support

--- js/bits.js
+++ js/bits.js
@@ -1,19 +1,11 @@
+(function(global) {
+
 var ws = new WebSocket("ws://bits.poul.org:8080/");
 
-ws.onmessage = function(event) {
-  openCloseHandler(event.data);
-}
-
-var openCloseHandler = function (openClose) {
-  if (openClose == "open") {
-    chrome.browserAction.setIcon({path: "img/open.jpg"});
-    chrome.browserAction.setTitle({title: "Poul Aperto"});
-  } else {
-    chrome.browserAction.setIcon({path: "img/close.jpg"});
-    chrome.browserAction.setTitle({title: "POuL Chiuso"});
-  }
-}
+ws.onmessage = wsMessageHandler;
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({url: "http://bits.poul.org"});
 });
+
+})(this)
