08d69da19ed605c18ff7f6e6581c6b6bc1540fa5
Andrea Brancaleoni <virus@gmx.us>
2012-01-10 16:51:37 +0100

    Working Chrome ext

--- js/bits.js
+++ js/bits.js
@@ -1,10 +1,10 @@
 (function(exports) {
 
-var ws = new WebSocket("ws://bits.poul.org:3389/");
+var ws = new WebSocket("ws://bits.poul.org:3389");
 
 var handler = new Handler(chromeHandler);
 
-ws.onmessage = function(event) { handler.webSocket(handler); };
+ws.onmessage = function(event) { handler.webSocket(event); };
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({url: "http://bits.poul.org"});
